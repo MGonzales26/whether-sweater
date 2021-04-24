908ebe058fde04aebb13e3d562bb45175f12e033
Megan Gonzales <megan.campbell26@yahoo.com>
2021-04-24 12:37:57 -0600

    Refactor background facade and service

---
+++ app/facades/background_facade.rb
@@ -0,0 +1,8 @@
+class BackgroundFacade
+
+  def self.get_background_image(location)
+    image = BackgroundService.get_background_image(location)
+    
+    Background.new(image, location)
+  end
+end
