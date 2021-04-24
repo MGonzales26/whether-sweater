643e8fce4737f2016952567ead661c85704863e4
Megan Gonzales <megan.campbell26@yahoo.com>
2021-04-24 16:10:05 -0600

    Create tests for sad path of user creation

--- spec/requests/api/v1/user_create_spec.rb
+++ spec/requests/api/v1/user_create_spec.rb
@@ -20,4 +20,61 @@ RSpec.describe 'user post endpoints' do
       expect(User.last.email).to eq('whatever@example.com')
     end
   end
+
+  describe 'sad path' do
+    it 'returns a 400 error if email exists' do
+        create(:user, email: 'whatever@example.com',  password: '1234', password_confirmation: '1234')
+  
+        user_info = {
+          email: 'whatever@example.com',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+  
+  
+        post '/api/v1/users', headers: { 'Content-Type' => 'application/'},
+        params: JSON.generate(user_info)
+  
+        expect(response.status).to eq(400)
+    end
+
+    it 'returns a 400 error if password confirmation does not match' do  
+        user_info = {
+          email: 'whatever@example.com',
+          password: 'password',
+          password_confirmation: 'word'
+        }
+  
+        post '/api/v1/users', headers: { 'Content-Type' => 'application/'},
+        params: JSON.generate(user_info)
+  
+        expect(response.status).to eq(400)
+    end
+
+    it 'returns a 400 error if email is empty' do  
+        user_info = {
+          email: '',
+          password: 'password',
+          password_confirmation: 'password'
+        }
+  
+        post '/api/v1/users', headers: { 'Content-Type' => 'application/'},
+        params: JSON.generate(user_info)
+  
+        expect(response.status).to eq(400)
+    end
+
+    it 'returns a 400 error if password is empty' do  
+        user_info = {
+          email: 'whatever@example.com',
+          password: '',
+          password_confirmation: 'password'
+        }
+  
+        post '/api/v1/users', headers: { 'Content-Type' => 'application/'},
+        params: JSON.generate(user_info)
+  
+        expect(response.status).to eq(400)
+    end
+  end
 end
